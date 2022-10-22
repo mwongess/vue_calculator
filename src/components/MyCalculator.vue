5a5055f7516ba9f3965cf621bf46c09a1a9734cf
4ouR04 <amosmwongelah@gmail.com>
2022-10-22 18:39:38 +0300

    Start a new project

---
+++ src/components/MyCalculator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCalculator',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
