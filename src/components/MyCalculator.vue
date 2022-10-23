bccd397ec5503260030a129b7913d9ee2fc82cf3
4ouR04 <amosmwongelah@gmail.com>
2022-10-23 22:12:49 +0300

    Fix adding . to case where it already exists

--- src/components/MyCalculator.vue
+++ src/components/MyCalculator.vue
@@ -5,20 +5,20 @@
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
     <div @click="append()" class="btn operator">÷</div>
-    <div @click="append()" class="btn">7</div>
-    <div @click="append()" class="btn">8</div>
-    <div @click="append()" class="btn">9</div>
+    <div @click="append(7)" class="btn">7</div>
+    <div @click="append(8)" class="btn">8</div>
+    <div @click="append(9)" class="btn">9</div>
     <div @click="append()" class="btn operator">x</div>
-    <div @click="append()" class="btn">4</div>
-    <div @click="append()" class="btn">5</div>
-    <div @click="append()" class="btn">6</div>
+    <div @click="append(4)" class="btn">4</div>
+    <div @click="append(5)" class="btn">5</div>
+    <div @click="append(6)" class="btn">6</div>
     <div @click="append()" class="btn operator">-</div>
-    <div @click="append()" class="btn">1</div>
-    <div @click="append()" class="btn">2</div>
-    <div @click="append()" class="btn">3</div>
+    <div @click="append(1)" class="btn">1</div>
+    <div @click="append(2)" class="btn">2</div>
+    <div @click="append(3)" class="btn">3</div>
     <div @click="append()" class="btn operator">+</div>
-    <div @click="append()" class="btn zero">0</div>
-    <div @click="append()" class="btn">.</div>
+    <div @click="append(0)" class="btn zero">0</div>
+    <div @click="dot()" class="btn">.</div>
     <div @click="append()" class="btn operator">=</div>
   </div>
 </template>
@@ -42,9 +42,18 @@ export default {
     percent(){
       this.current = `${parseFloat(this.current) / 100}`
     },
-    append(){
+    append(number){
+      this.current = `${this.current}${number}`
+    },
+    times(){
 
+    },
+    dot(){
+      if(this.current.indexOf('.') === -1 ){
+        this.append(".")
+      }
     }
+    
   }
 }
 </script>
